a5958012b733083037ab6d2c9a2089c6db0bf461
SabinoCarlucci <94316189+SabinoCarlucci@users.noreply.github.com>
2024-07-24 15:33:46 +0200

    Update ft_memcmp.c -- newline before #include

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -9,6 +9,7 @@
 /*   Updated: 2023/10/17 10:09:32 by scarlucc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
